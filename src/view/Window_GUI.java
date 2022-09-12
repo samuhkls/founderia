c3b841f566fd9223aa2efc3028bd5f882a624a56
Diogo Pereira <diogowork00@gmail.com>
2022-09-12 16:58:20 -0300

    000

--- src/view/Window_GUI.java
+++ src/view/Window_GUI.java
@@ -33,11 +33,11 @@ public class Window_GUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 685, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 431, Short.MAX_VALUE)
         );
 
         pack();
